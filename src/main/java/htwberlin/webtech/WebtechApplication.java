409f310bb7e438a55ddcff5e27d4afa8d631e72b
Maurice <s0556016@htw-berlin.de>
2024-04-19 19:55:25 +0200

    Import des SpringProjekts

---
+++ src/main/java/htwberlin/webtech/WebtechApplication.java
@@ -0,0 +1,15 @@
+package htwberlin.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
+public class WebtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechApplication.class, args);
+	}
+
+}
